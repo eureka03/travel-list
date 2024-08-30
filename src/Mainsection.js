d951a3c35ec4930cd86ba43cd45730f2f1e8b43a
Eureka Baloyi <eureka.baloyi@liquid.tech>
2024-08-30 16:03:06 +0200

    sort condition

--- src/Mainsection.js
+++ src/Mainsection.js
@@ -1,20 +1,33 @@
 import './Mainsection.css'
 import Item from './Item';
+import {useState} from 'react';
 
 
 export default function Mainsection({items,onDeleteItem,onToggleItem,onClearList}){
+    const [sortBy,setSortBy] = useState('input');
+    let sortedItems;
+
+    if(sortBy === 'input'){
+        sortedItems = items;
+    }else if(sortBy ==='description'){
+        sortedItems = (items.slice()).sort((a,b)=>(a.description).localeCompare(b.description));
+    }else if(sortBy ==='packed'){
+        
+        sortedItems = (items.slice()).sort((a,b)=>Number(a.packed)-Number(b.packed));
+
+    }
     return(
         <div className='mainsection'>
         
                 <ul>
-                    {(items).map(item=><Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id}/>)}
+                    {(sortedItems).map(item=><Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id}/>)}
                 </ul>
             
             <div className='bottomsection'>
-                <select>
-                    <option>SORT BY INPUT ORDER</option>
-                    <option>SORT BY DESCRIPTION</option>
-                    <option>SORT BY PACKED STATUS</option>
+                <select value={sortBy} onChange={(e)=>{setSortBy(e.target.value)}}>
+                    <option value='input'>SORT BY INPUT ORDER</option>
+                    <option value='description'>SORT BY DESCRIPTION</option>
+                    <option value='packed'>SORT BY PACKED STATUS</option>
                 </select>
                 <input type='reset' value='CLEAR LIST' onClick={onClearList}/>
             </div>
